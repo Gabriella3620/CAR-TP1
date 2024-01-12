665cdebf67fcc29ecbff789d0ec6e2f09e3c376d
Gabriella3620 <124586807+Gabriella3620@users.noreply.github.com>
2024-01-12 15:54:02 +0100

    Connexion réussie

--- Server.java
+++ Server.java
@@ -4,29 +4,29 @@ import java.util.*;
 public class Server{
 
         public static void main(String[] args) throws IOException  {
-            Socket serv = new ServerSocket(2121);
-            System.out.println("Serveur pret à accepter des connexions sur le  port 2121" + serverAddress + ":" + port);
+            ServerSocket serv = new ServerSocket(2121);
+            Socket s2 = serv.accept();
 
             try{ 
                 while (true){ 
-                    s2= serv.accept();
-                    System.out.println("Le client connecté.");
+                
+                    
+                    OutputStream out = s2.getOutputStream();
+                    
+                    String messageSend = "220 Service ready\r\n";
 
+                    InputStream in = s2.getInputStream();
+                    Scanner scan = new Scanner(in);
+                    String str = scan.nextLine();
+
+                    out.write(str.getBytes());
             }
             }
             finally {
             s2.close();
         }
             }
-            private static void sendMsgToClient(Socket s2) throws IOException {
-            InputStream in = Socket.getInputStream();
-            OutputStream out = Socket.getOutputStream();
-            Scanner scan = new Scanner(in);
 
-            String messageSend = "220 Service ready";
-            byte[] messageSendBytes = messageSend.getBytes();
-            out.write("messageSendBytes".getBytes());
-            /*à continuer*/
+        
             
-}
 }
