ad69b77f9c174c1427ee48319c7ddbfef8e52edf
Gabriella3620 <124586807+Gabriella3620@users.noreply.github.com>
2024-01-12 11:47:18 +0100

    Définition de la Classe et des variables et Création du Socket Serveur

--- Server.java
+++ Server.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class Server{
 
+        public static void main(String[] args) throws IOException  {
+            Socket serv = new ServerSocket(2121);
+            System.out.println("Serveur pret à accepter des connexions sur le  port 2121" + serverAddress + ":" + port);
+
+            try{ 
+                while (true){ 
+                    s2= serv.accept();
+                    System.out.println("Le client connecté.");
+
+            }
+            }
+            finally {
+            s2.close();
+        }
+            }
+            InputStream in = Socket.getInputStream();
+            OutputStream out = Socket.getOutputStream();
+            Scanner scan = new Scanner(in);
+            
+}
