40f3641e6511fc04281c0e7164ff3d72d0c5e318
Gabriella3620 <124586807+Gabriella3620@users.noreply.github.com>
2024-01-12 17:18:42 +0100

    ajout de la connexion avec login et mot de passe
    j'ai encore une erreur que j'essaie de corriger

--- Server.java
+++ Server.java
@@ -10,22 +10,58 @@ public class Server{
             try{ 
                 while (true){ 
                 
-                    
+                    InputStream in = s2.getInputStream();
+                    /* pour afficher le msg */ 
                     OutputStream out = s2.getOutputStream();
                     
                     String messageSend = "220 Service ready\r\n";
-
-                    InputStream in = s2.getInputStream();
                     Scanner scan = new Scanner(in);
                     String str = scan.nextLine();
 
                     out.write(str.getBytes());
+                    system.out.println(str);
+                    String user = "Gabriella";
+                    String password = "Miage";
+
+                    while (true){
+                        if(str.equals("user Name" + user)) {
+                            system.out.println("User name ok");
+                            String infoUser = "220 User valid \r\n";
+                            out.write(infoUser.getBytes());                          
+                        }
+                    String str2 = scan.nextLine();
+                    if(str2.equals ("Password" + password)){
+                        String userLogged = "220 User logged in \r\n";
+                       
+                        out.write(userLogged.getBytes());
+                        while (true){
+                            String str3 = scan.nextLine();
+                            if(str3.equals ("Quit")){
+                                String quit = "220 User logged out \r\n";
+                       
+                                out.write(userLogged.getBytes());
+                            
+                            }
+
+                    }
+
+                    }
+
+
+        
+            }
             }
             }
             finally {
             s2.close();
         }
-            }
+            
+
+        
+            
+}
+}
+
 
         
             
